8a138f2f537c21746e51b449a3783caa026613ee
jhair <hairgamer@live.com>
2026-04-25 19:21:15 -0400

    Only updating the ssh block in the shell script

--- scripts/set_env_vars.sh
+++ scripts/set_env_vars.sh
@@ -3,6 +3,6 @@
 
 MY_IP=$(curl -s checkip.amazonaws.com)
 export TF_VAR_allowed_ssh_cidrs='["'$MY_IP'/32"]'
-export TF_VAR_allowed_p4_cidrs='["'$MY_IP'/32"]'
 
-echo "Set SSH/P4 access to: $MY_IP/32"
+
+echo "Set SSH access to: $MY_IP/32"
